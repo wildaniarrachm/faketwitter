d31d49f3809301194d88138315543b72fdea0f4c
wildaniarrachm <132856014+wildaniarrachm@users.noreply.github.com>
2023-10-05 13:20:01 +0700

    fake-twitter

---
+++ src/components/register.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import {Formik, Form, ErrorMessage, Field} from 'formik'
+import * as Yup from "yup"
+
+
+export const FakeTwitter = () => {
+    
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import {Flex} from '@chakra-ui/react'
+
+ export const Home = () => {
+    return (
+        <Flex alignContent={'center'} flexDirection={'row'} >
+            <Flex>
+            <img style={{width:'100px'} } src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="logo" />
+            </Flex>
+            
+        </Flex>
+    )
+}
